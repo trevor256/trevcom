0e261155e18e743ce835b0b3fe210251daefe1fe
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 <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>
2022-03-27 20:14:37 -0500

    created more routes and mongodb and mongoose

--- routes/posts.js
+++ routes/posts.js
@@ -1,11 +1,27 @@
 const express = require('express');
+const Post = require( './../models/post');
 const router = express.Router()
 
-router.get('posts/new', (req, res) => {
-  res.send('posts/new')
+router.get('/new', (req, res) => {
+  res.render('posts/new', {post: new Post()})
 })
 
-router.post('/', (req, res) => {
+router.get('/:id', (req, res) => {
+  res.send(req.params.id)
+})
+
+router.post('/', async (req, res) => {
+  let post = new Post({
+    title: req.body.title,
+    description: req.body.description,
+    markdown: req.body.markdown
+  })
+  try {
+    post = await post.save()
+    res.redirect('/posts/${post.id}')
+  } catch (e) {
+    res.render('posts/new', {post: post})
+  }
 
 })
 
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const postsRouter = require('./routes/posts')
 const app = express()
 
-app.set('view engine', 'ejs')
-app.use('/post', postsRouter)
+mongoose.connect('mongodb://localhost/')
 
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     const post = [{
@@ -19,10 +22,14 @@ app.get('/', (req, res) => {
 
     },
     {
-      title: 'test post',
+      title: 'test post3',
       createdAt: new Date(),
       description: 'test desc'
     }]
   res.render('posts/index', { post: post})
 })
+
+
+app.use('/posts', postsRouter)
+
 app.listen(8080)
