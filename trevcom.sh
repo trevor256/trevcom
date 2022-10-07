4ed6a996674e6ab73c1d76e75be107d887df2ee4
trevor256 <256trevor@gmail.com>
2022-10-06 22:55:22 -0500

    servertest

---
+++ trevcom.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+curl -sL https://deb.nodesource.com/setup_18.x | sudo bash -
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install -y nodejs selinux-basics selinux-policy-default auditd
+# setsebool -P ssh_keysign 1
+# selinux-activate
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp -m tcp -m state -m comment -s xxx.xxx.xxx.xxx/32 --dport 22 --state NEW -j ACCEPT --comment "Open SSH Port for your xxx.xxx.xxx.xxx/32 IP only "
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+git clone https://github.com/trevor256/trevcom.git
+sudo reboot
+
