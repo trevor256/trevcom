78581df8f8d0ba878ecf327184f279d72dabdfa6
trevor256 <256trevor@gmail.com>
2023-03-09 13:58:46 -0600

    new

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ sudo apt update && sudo apt upgrade -y
 
 curl -fsSL https://deb.nodesource.com/setup_19.x | sudo bash - && sudo apt install -y nodejs
 sudo npm install --global yarn
+cd trevor256.com
 sudo yarn add express.js multer cors
-sudo yarn add -g nodemon
+sudo yarn global add nodemon
