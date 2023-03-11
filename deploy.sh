ba7238b2f49c463a0d87ea925f9dea953b5f8789
trevor256 <256trevor@gmail.com>
2023-03-11 06:04:49 -0600

    removed

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!\bin\sh
-sudo apt update && sudo apt upgrade -y
-
-curl -fsSL https://deb.nodesource.com/setup_19.x | sudo bash - && sudo apt install -y nodejs
-sudo npm install --global yarn
-cd trevor256.com
-sudo yarn add express.js multer cors
-sudo yarn global add nodemon
